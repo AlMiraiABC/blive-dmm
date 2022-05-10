870720a42bbaaa70a5e12cc555aa6fb34b7cc8b5
AlMirai <live.almirai@outlook.com>
2022-05-10 11:31:10 +0800

    feat: remove errors

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
 import asyncio
 
 from manager.config import Config
+from manager.logger import Logger
 from manager.room import Room
-from utils.logger import Logger
 
-app_config = Config().get_app()
+
+def init():
+    Config()
+
+
 logger = Logger(__file__).logger
 
 
 async def main():
     room_id = Config().get_room()['id']
+    app_monitor_interval = Config().get_app()['monitor']['interval']
     async with Room() as c:
         # keep running
         while True:
-            await asyncio.sleep(app_config['monitor']['interval'])
+            await asyncio.sleep(app_monitor_interval)
             s = await c.status()
             logger.info(f'Live room {room_id} is living {s}.')
             if not s[0]:
@@ -23,7 +28,10 @@ async def main():
 
 if __name__ == '__main__':
     try:
-        logger.info('Application starting.')
+        logger.info("Application initialization...")
+        init()
+        logger.info("Application initialized.")
+        logger.info('Application starting....')
         asyncio.run(main())
     except KeyboardInterrupt:
         pass
